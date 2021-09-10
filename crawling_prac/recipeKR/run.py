fbb1877d1f538d1574f0ab7c7896b8ef308f11cc
DasomOh <DasomOh88@gmail.com>
2021-09-10 13:08:09 +0900

    wsl lab17

--- crawling_prac/recipeKR/run.py
+++ crawling_prac/recipeKR/run.py
@@ -6,8 +6,8 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 try:
     execute(
         # ["scrapy", "crawl", "ssg"]
-        # ["scrapy", "crawl", "wtable"]
-        ["scrapy", "crawl", "manrecipe"]
+        ["scrapy", "crawl", "wtable"]
+        # ["scrapy", "crawl", "manrecipe"]
         # ["scrapy", "crawl", "quotes"]
     )
 except SystemExit:
