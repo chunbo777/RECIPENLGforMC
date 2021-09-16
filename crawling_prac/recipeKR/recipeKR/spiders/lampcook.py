5726f2098990e60ddadd72f59e5ea8362be42f62
chunbo777 <apppl2cation@naver.com>
2021-09-16 13:57:29 +0900

    north

--- crawling_prac/recipeKR/recipeKR/spiders/lampcook.py
+++ crawling_prac/recipeKR/recipeKR/spiders/lampcook.py
@@ -9,14 +9,16 @@ from bs4 import BeautifulSoup
 
 class LampcookSpider(CrawlSpider):
     name = 'lampcook'
-    allowed_domains = ['www.lampcook.com']
+    allowed_domains = ['lampcook.com']
     # start_urls = [f'http://www.lampcook.com/food/food_local_list.php?pagenum={i}' for i in range(1,326)]
-    start_urls = [f'http://lampcook.com/food/food_north_list.php?pagenum={i}' for i in range(1, 10)]
+    start_urls = [f'http://www.lampcook.com/food/food_north_list.php?pagenum={i}' for i in range(1, 2833)]
     
     rules = (
-        Rule(LinkExtractor(allow=r"/food/food_north_view"), callback='parse_item', follow=True , process_links="process_link", process_request="proreq"),
+        Rule(LinkExtractor(allow=r"food/food_north_view"), callback='parse_item', follow=True , process_links="process_link", process_request="proreq"),
     )
-
+    #allow=r"food/food_north_view"
+##div_main_content > div.item_box_data2 > ul:nth-child(1) > li.item_box_thum > a
+#<a href="/food/food_north_view.php?idx_no=2272" onclick="openPage(2272); return false;" title="뱀장어련뿌리호두볶음 레시피 바로가기 >"><img src="/wi_files/food_north_img/2272.jpg" alt="뱀장어련뿌리호두볶음" class="txt_vt"></a>
     # rules = (
     #     Rule(LinkExtractor(), callback='parse_page', follow=True, process_links="process_link"),
     # )
@@ -35,8 +37,11 @@ class LampcookSpider(CrawlSpider):
             
             
             title = soup.select_one('#div_main_content > ul.glo_ul_content > li:nth-child(2) > div > h1').text.strip()
-            ingredients=[i.strip() for i in soup.select_one("#div_main_content > div:nth-child(3) > div.padd20").text.split(",")] 
-            directions = [soup.select_one(f"#div_main_content > div:nth-child({i+4}) > div.padd20").text.strip() for i in range(len(soup.select("div.padd20"))-4)]
+            ingredients1=[i.strip() for i in soup.select_one("#div_main_content > div:nth-child(5) > div.padd20").text.split(",")] 
+            ingredients2=[i.strip() for i in soup.select_one("#div_main_content > div:nth-child(6) > div.padd20").text.split(",")] 
+            ingredients= ingredients1+ingredients2
+            
+            directions = [soup.select_one(f"#div_main_content > div:nth-child({i+7}) > div.padd20").text.strip() for i in range(len(soup.select("div.padd20"))-5)]
             
             # directions=soup.select(f"#div_main_content > div:nth-child(4) > div.padd20")
             # #div_main_content > div:nth-child(5) > div.padd20
